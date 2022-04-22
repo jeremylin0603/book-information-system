cb04ddbea2d2576981d1c97930cc16b7aa81cc4a
jeremy <jeremyl0603@gmail.com>
2022-04-23 02:05:16 +0800

    chore: 新增 sass 全局配置 @/styles/common.sass

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production' ? '/book-information-system' : '/',
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'sass',
+      patterns: ['./src/styles/common.sass']
+    }
+  }
 })
